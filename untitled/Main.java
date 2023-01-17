8ef98f52bff9841af0a563575281f97aedabc837
Okan Aktaş <okanaktas02@gmail.com>
2023-01-17 21:07:02 +0200

    Initial commit

---
+++ untitled/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+
+
+        Person person = new Person();
+
+        System.out.println("Hello World");
+
+    }
+}
---
+++ untitled/Persons.java
@@ -0,0 +1,36 @@
+public class Persons {
+    private String name;
+    private String job;
+    private int age;
+
+    public Persons(String name, String job, int age){
+        this.name = name;
+        this.job = job;
+        this.age = age;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
